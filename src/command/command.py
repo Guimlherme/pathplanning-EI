4a42a056049fdae72deef5bb3623d7697e5e92d9
Lucas José Velôso de Souza <60625769+SJVLucas@users.noreply.github.com>
2022-11-10 11:58:11 +0100

    Update command.py
    Control Law

--- src/command/command.py
+++ src/command/command.py
@@ -1,5 +1,7 @@
 RIGHT = 1
 LEFT = -1
+from constants import WHEEL_RADIUS,ROBOT_SPEED,ROBOT_WIDTH,OMEGA_MAX
+import numpy as np
 
 class Command:
     def get_name(self):
@@ -40,11 +42,36 @@ class RightTurn(Command):
 
 
 class Forward(Command):
+
     def __init__(self, actuators):
         self.actuators = actuators
+        self.previus_command = 0
+        self.command = 0
 
     def execute(self, state):
-        self.actuators.set_speeds(0.8, 0.8)
+
+        u = self.previus_command
+        u = 0.7482 * u + 9.2045*state.line_angle - 9.676 * state.previous_line_angle
+
+        self.previus_command= self.command
+        self.command = u
+
+        if u > 0:
+            w_right = (ROBOT_SPEED+((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
+            w_left = (ROBOT_SPEED-((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
+
+            if w_right > OMEGA_MAX:
+                w_right = OMEGA_MAX
+                w_left = (1/WHEEL_RADIUS)*(ROBOT_SPEED*u)/(u+ROBOT_SPEED*ROBOT_WIDTH)
+        else:
+            u = np.abs(u)
+            w_right = (ROBOT_SPEED-((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
+            w_left = (ROBOT_SPEED+((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
+
+            if w_left > OMEGA_MAX:
+                w_left = OMEGA_MAX
+                w_right = (1/WHEEL_RADIUS)*(ROBOT_SPEED*u)/(u+ROBOT_SPEED*ROBOT_WIDTH)
+        self.actuators.set_speeds(w_right, w_left)
     
     def get_name(self):
         return "Forward"
