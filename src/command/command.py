3ad0486696aad91a5df051202d55bb8b9e5ca584
alexandre <alexandremr01@gmail.com>
2022-11-10 12:16:55 +0100

    simplify control

--- src/command/command.py
+++ src/command/command.py
@@ -56,28 +56,28 @@ class Forward(Command):
 
     def execute(self, state):
 
-        u = self.previus_command
-        u = 0.7482 * u + 9.2045*state.line_angle - 9.676 * state.previous_line_angle
+        # u = self.previus_command
+        # u = 0.7482 * u + 9.2045*state.line_angle - 9.676 * state.previous_line_angle
 
-        self.previus_command= self.command
-        self.command = u
+        # self.previus_command= self.command
+        # self.command = u
 
-        if u > 0:
-            w_right = (ROBOT_SPEED+((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
-            w_left = (ROBOT_SPEED-((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
+        # if u > 0:
+        #     w_right = (ROBOT_SPEED+((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
+        #     w_left = (ROBOT_SPEED-((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
 
-            if w_right > OMEGA_MAX:
-                w_right = OMEGA_MAX
-                w_left = (1/WHEEL_RADIUS)*(ROBOT_SPEED*u)/(u+ROBOT_SPEED*ROBOT_WIDTH)
-        else:
-            u = np.abs(u)
-            w_right = (ROBOT_SPEED-((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
-            w_left = (ROBOT_SPEED+((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
+        #     if w_right > OMEGA_MAX:
+        #         w_right = OMEGA_MAX
+        #         w_left = (1/WHEEL_RADIUS)*(ROBOT_SPEED*u)/(u+ROBOT_SPEED*ROBOT_WIDTH)
+        # else:
+        #     u = np.abs(u)
+        #     w_right = (ROBOT_SPEED-((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
+        #     w_left = (ROBOT_SPEED+((ROBOT_WIDTH/2)*u))/WHEEL_RADIUS
 
-            if w_left > OMEGA_MAX:
-                w_left = OMEGA_MAX
-                w_right = (1/WHEEL_RADIUS)*(ROBOT_SPEED*u)/(u+ROBOT_SPEED*ROBOT_WIDTH)
-
+        #     if w_left > OMEGA_MAX:
+        #         w_left = OMEGA_MAX
+        #         w_right = (1/WHEEL_RADIUS)*(ROBOT_SPEED*u)/(u+ROBOT_SPEED*ROBOT_WIDTH)
+        w_right, w_left = 0.8*OMEGA_MAX, 0.8*OMEGA_MAX
         state.right_wheel_command = w_right
         state.left_wheel_command = w_left
         print("Left wheel speed: ", w_left)
