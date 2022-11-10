21a5d807e0949fb6221d41ec8a7c03b7708bf209
Lucas José Velôso de Souza <60625769+SJVLucas@users.noreply.github.com>
2022-11-10 14:07:24 +0100

    Update constants.py
    Adding more control constants

--- src/constants.py
+++ src/constants.py
@@ -11,3 +11,15 @@ ROBOT_WIDTH = 0.152 # m
 ROBOT_SPEED = 0.22 # m/s
 WHEEL_RADIUS = 0.0325 # m
 OMEGA_MAX = 10.00 # rad/s
+
+# CONTROL LAW PARAMETERS
+XI = 0.70
+WN = 6.00      # rad/s
+TAU = 0.600    # s
+TD = 0.015     # s - time of discretization
+
+CONTROL_PARAMETERS = {
+     'phi':{'k':(2*(TAU-TD)*(WN**2))/(4+TD*WN*(4*XI+TAU*WN)),
+            'k-1':-(2*(TAU+TD)*(WN**2))/(4+TD*WN*(4*XI+TAU*WN))},
+     'u':{'k-1':(4-TD*WN*(4*XI+TAU*WN))/(4+TD*WN*(4*XI+TAU*WN))},
+}
