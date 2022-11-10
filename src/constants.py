40ecb716ecff39ae87089305fc8df3e6c874c556
Guilherme <guilhermeoliveira5001@gmail.com>
2022-11-10 13:30:41 +0100

    Fixed wheel distance constant

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ import numpy as np
 CYCLE_TIME = 15e-3 # s
 DISTANCE_THRESHOLD = 15 # cm
 ANGLE_THRESHOLD = np.deg2rad(5) # rad
-WHEEL_DIST = 15 # centimeters
+WHEEL_DIST = 15.2 # centimeters
 ROBOT_WIDTH = 0.152 # m
 ROBOT_SPEED = 0.18 # m/s
 WHEEL_RADIUS = 0.0325 # m
