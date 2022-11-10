d0cdaa69dab118efe05b531d048625a6be8890a4
Guilherme <guilhermeoliveira5001@gmail.com>
2022-11-10 10:23:42 +0100

    Now we (corretly) print tilted cars

--- src/perception/map.py
+++ src/perception/map.py
@@ -1,6 +1,7 @@
 from math import sqrt
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
+from numpy import deg2rad
+#import matplotlib.pyplot as plt
+#from matplotlib.patches import Rectangle
 
 class Map:
     def __init__(self):
@@ -62,15 +63,22 @@ class Map:
         plt.show()
 
     def print_with_robot(self, state):
-        self.print()
-        width = 0.25
-        height = 0.5
+        plt.figure()
+        for node in self.nodes:
+            plt.scatter(*self.nodes[node], c='#0000FF')
+            for neighbor in self.adjacency_list[node]:
+                # print(self.nodes[node][0])
+                plt.plot([self.nodes[node][0], self.nodes[neighbor][0]], [self.nodes[node][1], self.nodes[neighbor][1]],
+                         c='#FF0000')
+        width = 0.1
+        height = 0.3
         plt.gca().add_patch(Rectangle((state.x-width/2, state.y-height/2), width, height,
-                                      angle=state.theta,
-                                      edgecolor='red',
-                                      facecolor='red',
+                                      angle=rad2deg(state.theta),
+                                      edgecolor='green',
+                                      facecolor='green',
                                       lw=4,
                                       rotation_point='center'))
+        plt.show()
 
 
 
