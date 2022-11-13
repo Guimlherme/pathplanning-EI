5f53a42371ccb66f1af5d284d6638b94b409fe7e
Lucas José Velôso de Souza <60625769+SJVLucas@users.noreply.github.com>
2022-11-13 23:47:31 +0100

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,17 +1,21 @@
 import numpy as np
 
 # Values used for real robot
-# CYCLE_TIME = 120e-3 # s
-# DISTANCE_THRESHOLD = 20 # cm
+CYCLE_TIME = 120e-3 # s
+DISTANCE_THRESHOLD = 20 # cm
 
 # Values used for simulation
-CYCLE_TIME = 30e-3 # s
-DISTANCE_THRESHOLD = 5 # cm
+# CYCLE_TIME = 30e-3 # s
+# DISTANCE_THRESHOLD = 5 # cm
 
 FINISH_TURN_ANGLE_THRESHOLD = np.deg2rad(5) 
 TURN_ANGLE_THRESHOLD = np.deg2rad(35)
 
 WHEEL_DIST = 15.2 # centimeters
+
+ONE_LINE_MAP = False
+MINIMUM_AREA = 600
+DISTANCE_BETWEEN_LINES = 200
 
 DEL_SPEED_DEL_PSI = 0.06
 ROBOT_WIDTH = 0.152 # m
