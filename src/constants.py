45ee9f5975de614d81ebace6ff19304c1b09d180
Guilherme <guilhermeoliveira5001@gmail.com>
2022-11-13 02:34:22 +0100

    Tuned constants

--- src/constants.py
+++ src/constants.py
@@ -8,14 +8,13 @@ TURN_ANGLE_THRESHOLD = np.deg2rad(35)
 WHEEL_DIST = 15.2 # centimeters
 
 ROBOT_WIDTH = 0.152 # m
-ROBOT_SPEED = 0.22 # m/s
+ROBOT_SPEED = 0.16 # m/s
 WHEEL_RADIUS = 0.0325 # m
 OMEGA_MAX = 9.50 # rad/s
-OMEGA_NON_LINEAR = 7.5 # rad/s
 
 # CONTROL LAW PARAMETERS
 XI = 0.70
-WN = 6.00      # rad/s
+WN = 7.00      # rad/s
 TAU = 0.615    # s
 TD = CYCLE_TIME     # s - time of discretization
 
