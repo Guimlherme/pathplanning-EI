8e7124953e2f053188bb1e47a5f23d7563995c60
alexandre <alexandremr01@gmail.com>
2022-11-13 19:27:39 +0100

    separate real and simulation constants

--- src/astar.py
+++ src/astar.py
@@ -4,7 +4,6 @@ from perception.astar import find_path
 world_map = get_grid_map()
 world_map.remove_edge(6,11)
 world_map.remove_edge(5,10)
-# world_map.print()
 
 path = find_path(world_map, 1, 13)
 
--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+# Values used for real robot
+# CYCLE_TIME = 120e-3 # s
+# DISTANCE_THRESHOLD = 10 # cm
+
+# Values used for simulation
 CYCLE_TIME = 30e-3 # s
 DISTANCE_THRESHOLD = 20 # cm
 
@@ -24,6 +29,3 @@ CONTROL_PARAMETERS = {
             'k-1':-(2*(TAU+TD)*(WN**2))/(4+TD*WN*(4*XI+TAU*WN))},
      'u':{'k-1':(4-TD*WN*(4*XI+TAU*WN))/(4+TD*WN*(4*XI+TAU*WN))},
 }
-# 0 1 2 3
-# 5 6 7 8 9
-# 0 1 2 3 4
