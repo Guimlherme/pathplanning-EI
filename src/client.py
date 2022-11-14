81fb4f4760a81fc0ee866976b8256153999a2553
alexandre <alexandremr01@gmail.com>
2022-11-14 09:19:57 +0100

    enhance client

--- src/client.py
+++ src/client.py
@@ -5,21 +5,27 @@ from maps import get_grid_map, get_quarto_map
 import sys
 import select
 
+MAX_RETRIES = 9
+INITIAL_TIME = 0.3
+
 def get_connection(host, port):
     client_socket = socket.socket()  
-    t = 0.2
+    t = INITIAL_TIME
     count = 1
     connected = False
-    while not connected and count <= 5:
+    while not connected:
         print(f"Trying to connect, attempt #{count}")
         try:
             client_socket.connect((host, port))  
             connected = True
         except socket.error:
+            if count == MAX_RETRIES:
+                break
             print("Connection failed, retrying")
             time.sleep(t)
+            if t < 2:
+                t *= 2
             count += 1
-            t *= 2
     if not connected:
         raise Exception('Could not connect')
     return client_socket
@@ -33,6 +39,9 @@ def client_program(config, map):
     message = input(" -> ")
 
     while message.lower().strip() != 'quit':
+        if len(message) == 0:
+            message = input(" -> ") 
+            continue
         client_socket.send(message.encode()) 
         data = client_socket.recv(1024).decode()
         if message == 'u':
